e8410a7989468e19a43efc6d03425251964157a4
Braulio <brauliortegabatalla@gmail.com>
2025-04-30 19:03:25 -0500

    fix de dependencias de router REACT

--- gestion-APP/src/routes/AppRouter.tsx
+++ gestion-APP/src/routes/AppRouter.tsx
@@ -3,21 +3,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LoginPage } from '../modules/auth/LoginPage';
 import { RegisterPage } from '../modules/auth/RegisterPage';
 import { Navbar } from '../components/Navbar';
+import { DashboardPage } from '../modules/dashboard/DashboardPage';
 
 export const AppRouter = () => {
   const isAuthenticated = localStorage.getItem('auth') === 'true';
 
   return (
     <BrowserRouter>
-    <Navbar />
+    <Navbar/>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         
         {/* Ruta protegida de ejemplo */}
         <Route
-          path="/dashboard"
-          element={isAuthenticated ? <h1>Bienvenido al Dashboard</h1> : <Navigate to="/login" />}
+        path="/dashboard"
+        element={isAuthenticated ? <DashboardPage /> : <Navigate to="/login" />}
         />
 
         {/* Ruta por defecto */}
