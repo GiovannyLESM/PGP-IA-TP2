3336bcafc01c65f8e4946169fd5acbe727f115c3
Braulio <brauliortegabatalla@gmail.com>
2025-05-08 13:06:47 -0500

    configuracion tailwind tema oscuro / agregar boton de tema oscuro al nav / funcionamiento del boton en el nav / creacion del layout para reutilizar el tema oscuro

---
+++ gestion-APP/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Habilita el modo oscuro mediante la clase 'dark'
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
