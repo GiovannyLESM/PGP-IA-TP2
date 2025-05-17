7602dad08bff67833951d39bfe8be6c678c31a39
Braulio <brauliortegabatalla@gmail.com>
2025-05-17 17:06:05 -0500

    agregando rutas de la card - EditarTarjeta/asignar miembros/agregarItem a la checkList/ actualizacion del estado de la checklist / eliminar un item de la checkList

--- backend/routes/card.routes.js
+++ backend/routes/card.routes.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { crearTarjeta } from '../controllers/card.controller.js';
+import { 
+    crearTarjeta, 
+    editarTarjeta, 
+    asignarMiembros, 
+    agregarChecklistItem, 
+    actualizarChecklistItem, 
+        eliminarChecklistItem } from '../controllers/card.controller.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/listas/:id/tarjetas', protect, crearTarjeta); // POST /api/listas/:id/tarjetas
+router.put('/cards/:id', protect, editarTarjeta);
+router.put('/cards/:id/assign', protect, asignarMiembros);
+router.patch('/cards/:id/checklist', protect, agregarChecklistItem);
+router.patch('/cards/:cardId/checklist/:index', protect, actualizarChecklistItem);
+router.delete('/cards/:cardId/checklist/:index', protect, eliminarChecklistItem);
 
 export default router;
+
