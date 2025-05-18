8c42f305384fb434298d6b2de52bb0d85958c5d1
Braulio <brauliortegabatalla@gmail.com>
2025-05-18 18:25:51 -0500

    terminado la API de las Cards add/del adjuntos(links), add/del agregar etiqueta

--- backend/routes/card.routes.js
+++ backend/routes/card.routes.js
@@ -5,7 +5,10 @@ import {
     asignarMiembros, 
     agregarChecklistItem, 
     actualizarChecklistItem, 
-        eliminarChecklistItem } from '../controllers/card.controller.js';
+    eliminarChecklistItem, 
+    agregarAdjunto, 
+    eliminarAdjunto, 
+    agregarEtiqueta, eliminarEtiqueta } from '../controllers/card.controller.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -16,6 +19,10 @@ router.put('/cards/:id/assign', protect, asignarMiembros);
 router.patch('/cards/:id/checklist', protect, agregarChecklistItem);
 router.patch('/cards/:cardId/checklist/:index', protect, actualizarChecklistItem);
 router.delete('/cards/:cardId/checklist/:index', protect, eliminarChecklistItem);
+router.patch('/cards/:id/adjuntos', protect, agregarAdjunto);
+router.delete('/cards/:id/adjuntos/:index', protect, eliminarAdjunto);
+router.patch('/cards/:id/etiquetas', protect, agregarEtiqueta);
+router.delete('/cards/:id/etiquetas/:index', protect, eliminarEtiqueta);
 
 export default router;
 
