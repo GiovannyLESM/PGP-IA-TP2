4ed1303232ca1db44b3d63e3e47ba4015cd80512
Braulio <brauliortegabatalla@gmail.com>
2025-06-26 10:36:36 -0500

    aumentando limite express 10mb

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ import { User } from './models/User.js';
 dotenv.config();
 
 const app = express();
-
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api', listRoutes);
